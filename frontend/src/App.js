4b8242a9c87c20156e029c443ac096a4f79039fe
Jesús García González <jgrcglez@gmail.com>
2021-09-28 11:35:43 -0400

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /**
+     * @param response.accessToken
+     */
+    const responseFacebook = async (response) => {
+        await axios.post(
+            "/dj-rest-auth/facebook/",
+            {
+                access_token: response.accessToken,
+            }
+        ).then((res) => {
+            console.log(res.data.key);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <h1>LOGIN WITH FACEBOOK</h1>
+                <FacebookLogin
+                    textButton="LOGIN WITH FACEBOOK"
+                    appId={process.env.REACT_APP_APP_ID} //better get from DB (?)
+                    fields="name,email"
+                    callback={responseFacebook}
+                />
+            </header>
+        </div>
+    );
+
 }
 
 export default App;

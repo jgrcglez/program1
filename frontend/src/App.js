28afbf0d780396bb8e2823b8b8da35ebe8bfa38c
Jesús García González <jgrcglez@gmail.com>
2021-09-28 14:49:08 -0400

    FB login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from "react-google-login";
 import axios from "axios";
 
 /**
@@ -9,6 +10,22 @@ import axios from "axios";
 const responseFacebook = async (response) => {
     await axios.post(
         "/dj-rest-auth/facebook/",
+        {
+            access_token: response.accessToken,
+        }
+    ).then((res) => {
+        console.log(res.data.key);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+/**
+ * @param response.accessToken
+ */
+const responseGoogle = async (response) => {
+    await axios.post(
+        "/dj-rest-auth/google/",
         {
             access_token: response.accessToken,
         }
@@ -38,9 +55,17 @@ function App() {
                 <h1>LOGIN WITH FACEBOOK</h1>
                 <FacebookLogin
                     textButton="LOGIN WITH FACEBOOK"
-                    appId={process.env.REACT_APP_APP_ID} //better get from DB (?)
+                    appId={process.env.REACT_APP_FACEBOOK_APP_ID} //better get from DB (?)
                     fields="name,email"
                     callback={responseFacebook}
+                />
+                <h1>LOGIN WITH GOOGLE</h1>
+                <GoogleLogin
+                    clientId={process.env.REACT_APP_GOOGLE_APP_ID}
+                    buttonText="LOGIN WITH GOOGLE"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
                 />
             </header>
         </div>
